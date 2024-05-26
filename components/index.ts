d9cdfddc693f836370fb2b4c8523fbcfbc6dd396
Shivam Chauhan <shivamchauhan2463@gmail.com>
2024-05-26 20:31:26 +0530

    car card and details using car api

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
@@ -7,6 +9,8 @@ import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
 
 export {
+  CarCard,
+  CarDetails,
   CustomButton,
   CustomFilter,
   Footer,
